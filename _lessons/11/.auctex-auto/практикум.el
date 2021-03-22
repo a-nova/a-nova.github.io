8aeafb034a119a3e4828ab1e789211909b0dd2f8
Pavel Stepanov <pavelstepanov@vivaldi.net>
2021-03-22 08:55:39 +0600

    lesson 11 sem

---
+++ _lessons/11/.auctex-auto/практикум.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "практикум"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem") ("geometry" "a4paper" "margin=2cm")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry"
+    "fontspec"
+    "indentfirst"
+    "enumitem")
+   (LaTeX-add-labels
+    "sec:orgd88ec8e"
+    "sec:orgffca774"
+    "sec:orgcc51a96"
+    "sec:org57ff121"
+    "sec:org7637287"
+    "sec:org80c0562"
+    "sec:org403f09e"
+    "sec:orge85be41"
+    "sec:org00e864a"
+    "sec:orgb703d10"
+    "sec:orgddd8fb3"))
+ :latex)
+
